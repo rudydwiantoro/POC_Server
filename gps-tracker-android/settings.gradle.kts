e0d9bc82edda69ba0daf52fc50b0e9745a4d1694
owenltm <owenlwiantoro01@gmail.com>
2026-05-10 21:53:29 +0800

    feat: fix error

--- gps-tracker-android/settings.gradle.kts
+++ gps-tracker-android/settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(org.gradle.api.initialization.dsl.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
